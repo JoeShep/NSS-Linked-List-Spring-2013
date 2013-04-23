ba3e735b5e5990c40581dd4a8e486316939415ec
Joe Shepherd <joeshepmedia@gmail.com>
2013-04-23 11:34:37 -0500

    Add completed files. Need refactoring

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,18 +7,61 @@ class LinkedList
   end
 
   def add_item(payload)
+    next_item = LinkedListItem.new(payload)
+    if @first_item.nil?
+      @first_item = next_item
+    else
+      final_item = @first_item
+    until final_item.last?
+        final_item = final_item.next_list_item
+    end
+      final_item.next_list_item = next_item
+    end
   end
 
   def get(n)
+      i=0
+      next_item= @first_item
+        if next_item.nil?
+        raise IndexError
+      else
+        while i < n
+        next_item= next_item.next_list_item
+        i+=1
+     end
+    end
+   next_item.payload
   end
 
   def last
+      item=@first_item
+      return nil if item.nil?
+      until item.last?
+      item=item.next_list_item
+    end
+    item.payload
   end
 
   def size
+    item=@first_item
+    size=0
+    until item.nil?
+    item=item.next_list_item
+    size += 1
+   end
+    size
   end
 
   def to_s
+    current_item=@first_item
+    payloads = ""
+   while current_item
+     payloads += " "
+     payloads += current_item.payload
+     payloads += "," unless current_item.last?
+     current_item = current_item.next_list_item
+   end
+    "|#{payloads} |"
   end
 
   # ========= Bonus ========== #
--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -2,12 +2,16 @@ class LinkedListItem
   attr_reader :next_list_item
   attr_reader :payload
 
-  def initialize( payload )
+  def initialize( whatever )
+  	@payload=whatever
   end
 
   def next_list_item= (linked_list_item)
+  	raise ArgumentError unless linked_list_item != self
+  	@next_list_item=linked_list_item
   end
 
   def last?
+  	@next_list_item.nil?
   end
 end
